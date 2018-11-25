bdf746c30425d0ac9257ed6c57101641709dee25
sprkzoff <sprkzoff@gmail.com>
2018-11-25 20:53:12 +0700

    add owner/ getter & setter

--- src/pane/CharacterField.java
+++ src/pane/CharacterField.java
@@ -2,6 +2,9 @@ package pane;
 
 import javafx.scene.control.ProgressBar;
 
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
 import javax.swing.event.ChangeListener;
 
 import character.Character;
@@ -10,12 +13,14 @@ import javafx.scene.layout.HBox;
 import javafx.scene.layout.VBox;
 import javafx.scene.text.Text;
 
-public class CharacterField extends HBox {
+public class CharacterField extends HBox{
 	private Text name;
 	private Text hp;
 	private ColoredProgressBar hpBar;
+	private Character Owner;
 	
 	public CharacterField(Character character) {
+		Owner = character;
 		name = new Text(" "+character.getInstance().toString()+" ");
 		hp = new Text(" "+Integer.toString(character.getHp())+"/"+Integer.toString(character.MAX_HP));
 		hpBar = new ColoredProgressBar("green-bar",1);
@@ -24,6 +29,38 @@ public class CharacterField extends HBox {
 		getStylesheets().add(getClass().getResource("progress.css").toExternalForm());
 		
 		getChildren().addAll(name,hpBar,hp);
-	} 
+	}
+
+	public Text getName() {
+		return name;
+	}
+
+	public void setName(Text name) {
+		this.name = name;
+	}
+
+	public Text getHp() {
+		return hp;
+	}
+
+	public void setHp(Text hp) {
+		this.hp = hp;
+	}
+
+	public ColoredProgressBar getHpBar() {
+		return hpBar;
+	}
+
+	public void setHpBar(ColoredProgressBar hpBar) {
+		this.hpBar = hpBar;
+	}
+
+	public Character getOwner() {
+		return Owner;
+	}
+
+	public void setOwner(Character owner) {
+		Owner = owner;
+	}
 }
 
